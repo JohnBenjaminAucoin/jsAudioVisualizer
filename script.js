4fb7008bcde8a712f73b509b6a54daee4c8a798f
Benjamin Aucoin <43827051+JohnBenjaminAucoin@users.noreply.github.com>
2023-09-17 08:16:33 -0400

    added presets

--- script.js
+++ script.js
@@ -3,20 +3,88 @@ const drawVisualizer = ({
     dataArray,
     barWidth,
     amplitude,
-    size
+    size,
+    presetN
 }) => {
+    let args = {bufferLength,
+        dataArray,
+        barWidth,
+        amplitude,
+        size}
+        
+        switch(presetN){
+            case 1 :
+                preset1(args)
+            case 2 :
+                preset2(args)
+            case 3 :
+                preset3(args)
+        }
+        
+};
+
+function preset1({
+    bufferLength,
+    dataArray,
+    barWidth,
+    amplitude,
+    size
+}){
     let barHeight;
 
-    for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i] * amplitude;
-        const red =  i/size * 256 ;
-        const green = 256;
-        const blue = (1 - i/size) * 256;
-        ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
-        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight); // this will continue moving from left to right
-        x += barWidth; // increases the x value by the width of the bar
-    }
-};
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] * amplitude;
+            const red =  (1 - i/size)  * 256 ;
+            const green =i/size *  256;
+            const blue =  256;
+            ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight); // this will continue moving from left to right
+            x += barWidth; // increases the x value by the width of the bar
+        }
+
+}function preset2({
+    bufferLength,
+    dataArray,
+    barWidth,
+    amplitude,
+    size
+}){
+    let barHeight;
+
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i] * amplitude;
+            const red =  i/size * 256 ;
+            const green =  256;
+            const blue =  (1 - i/size) * 256;
+            ctx.strokeStyle = `rgb(${red}, ${green}, ${blue})`;
+            ctx.fillRect(x, canvas.height -barHeight, barWidth, 20); // this will continue moving from left to right
+            x += barWidth; // increases the x value by the width of the bar
+        }
+
+}
+
+function preset3({
+    bufferLength,
+    dataArray,
+    barWidth,
+    amplitude,
+    size
+}){
+    let barHeight;
+
+        for (let i = 0; i < bufferLength; i++) {
+            barHeight = dataArray[i];
+            const red =  i/size * 256 ;
+            const green =  256;
+            const blue =  (1 - i/size) * 256;
+            ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+            ctx.beginPath();
+            ctx.arc(canvas.width/2, canvas.height/2, barHeight, 0, 2 * Math.PI);
+            ctx.stroke();
+        }
+
+}
+
 
 
 let audio1 = new Audio();
@@ -41,28 +109,36 @@ analyser = audioCtx.createAnalyser();
 audioSource.connect(analyser);
 analyser.connect(audioCtx.destination);
 
-analyser.fftSize = 8192;
+analyser.fftSize = 256;
 const size = analyser.fftSize;
 const amplitude = 3.5;
+let presetN = 1;
+
 
 const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
 const barWidth = canvas.width / bufferLength;
+
+const EplayedBar = document.getElementById("played-bar")
 
 let x = 0;
 function animate() {
     x = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     analyser.getByteFrequencyData(dataArray);
+    
     drawVisualizer({
         bufferLength,
         dataArray,
         barWidth,
         amplitude,
-        size
+        size,
+        presetN
     });
 
-    console.log(audio1.duration)
+    EplayedBar.setAttribute("style",`width: ${audio1.currentTime/audio1.duration *100}%`)
+    
+
 
     requestAnimationFrame(animate);
 }
@@ -73,9 +149,10 @@ animate();
 
 
 function playbutton(){
-
     audioCtx.resume()
     audio1.play()
+}
 
-    }
-
+function choosePreset(number){
+    presetN = number;
+}
